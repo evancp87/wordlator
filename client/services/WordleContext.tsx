b93a059f3870be1ebf707587fe185290b7e3f984
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-03-07 00:16:02 +0000

    tweaks to context default ts values- needs more work

--- client/services/WordleContext.tsx
+++ client/services/WordleContext.tsx
@@ -1,44 +1,145 @@
-import { createContext} from 'react';
+import { createContext } from "react";
 
-export interface WordleContextValue {
+// export interface WordleContextValue {
+//   gameState: {
+//     isStarted: boolean;
+//     words: Array<string> | undefined;
+//     board: [
+//       [
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string }
+//       ],
+//       [
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string }
+//       ],
+//       [
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string }
+//       ],
+//       [
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string }
+//       ],
+//       [
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string }
+//       ],
+//       [
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string },
+//         { letter: string; status: string }
+//       ]
+//     ];
+//     currentRow: number;
+//     currentWord: string;
+//     currentGuess: string;
+//     error: string;
+//     gameOver: boolean;
+//   };
+// }
 
-isStarted: boolean;
-words: Array<string> | undefined,
-board: [
-    [{letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}],
-    [{letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}],
-    [{letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}],
-    [{letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}],
-    [{letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}],
-    [{letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}, {letter: string, status: string}]
-],
-currentRow: number,
-currentWord: string,
-currentGuess: string,
-error: string,
-gameOver: boolean,
+export type WordleContextValue = {
+  gameState: {
+    isStarted: boolean;
+    words: string[];
+    board: {
+      letter: string;
+      status: string;
+    }[][];
+    currentRow: number;
+    currentWord: string;
+    currentGuess: string;
+    error: string;
+    gameOver: boolean;
+  };
+  setGameState: React.Dispatch<
+    React.SetStateAction<{
+      isStarted: boolean;
+      words: string[];
+      board: {
+        letter: string;
+        status: string;
+      }[][];
+      currentRow: number;
+      currentWord: string;
+      currentGuess: string;
+      error: string;
+      gameOver: boolean;
+    }>
+  >;
+};
 
-}
-
-
-
-export const WordleContext = createContext<WordleContextValue>(
-{ 
-isStarted: false,
-words: [],
-board: [
-    [{letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}],
-    [{letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}],
-    [{letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}],
-    [{letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}],
-    [{letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}],
-    [{letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}, {letter: "", status: "white"}]
-],
-currentRow: 0,
-currentWord: "",
-currentGuess: "",
-error: String.fromCharCode(160),
-gameOver: false});
-
-
-
+export const WordleContext = createContext<WordleContextValue>({
+  gameState: {
+    isStarted: false,
+    words: [],
+    board: [
+      [
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+      ],
+      [
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+      ],
+      [
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+      ],
+      [
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+      ],
+      [
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+      ],
+      [
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+        { letter: "", status: "white" },
+      ],
+    ],
+    currentRow: 0,
+    currentWord: "",
+    currentGuess: "",
+    error: String.fromCharCode(160),
+    gameOver: false,
+  },
+  setGameState: () => {},
+});
--- client/services/WordleState.tsx
+++ client/services/WordleState.tsx
@@ -1,36 +1,71 @@
-import { createContext, useState} from 'react';
+import { createContext, useState } from "react";
 // import {WordleContext, WordleContextValue} from "./WordleContext";
-import {WordleContext, WordleContextValue} from "./WordleContext";
+import { WordleContext, WordleContextValue } from "./WordleContext";
 
 type WordleContextProps = {
-    children: React.ReactNode
+  children: React.ReactNode;
+};
+
+export function WordleProvider({ children }: WordleContextProps) {
+  const [gameState, setGameState] = useState<WordleContextValue>({
+    gameState: {
+      isStarted: false,
+      words: [],
+      board: [
+        [
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+        ],
+        [
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+        ],
+        [
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+        ],
+        [
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+        ],
+        [
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+        ],
+        [
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+          { letter: "", status: "white" },
+        ],
+      ],
+      currentRow: 0,
+      currentWord: "",
+      currentGuess: "",
+      error: String.fromCharCode(160),
+      gameOver: false,
+    },
+    setGameState: () => {},
+  });
+  return (
+    <WordleContext.Provider value={{ gameState, setGameState }}>
+      {children}
+    </WordleContext.Provider>
+  );
 }
-
-
-export function WordleProvider ({children}: WordleContextProps) {
-    const [gameState, setGameState] = useState<WordleContextValue>({
-        
-        isStarted: false,
-        words: [],
-        board:[
-            [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
-            [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
-            [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
-            [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
-            [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
-            [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}]
-            ],
-        currentRow: 0,
-        currentWord: "",
-        currentGuess: "",
-        error: String.fromCharCode(160),
-        gameOver: false,
-      });
-    return (
-        <WordleContext.Provider value={{gameState, setGameState}}>
-            {children}
-        </WordleContext.Provider>
-    )
-}
-
-
